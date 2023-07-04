fffcdb03e4a93fc7eeee97f68e958ec0105918ff
ornrm <rujirangorn@gmail.com>
2023-07-04 19:11:55 +0700

    s

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/homepage.vue'
 
 export default {
   name: 'App',
@@ -13,14 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
